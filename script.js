98b84a56ac468318d578ccb32a38d19f6a4d4aed
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-15 14:14:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const submitBtn = document.getElementById("submit-btn");
-const searchBar = document.getElementById("search-bar");
 const audioPlayer = document.getElementById("audio-player");
 
 submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
-  const searchQuery = searchBar.value;
-  const googleMapsUrl = `https://www.google.com/maps/search/${searchQuery}`;
   audioPlayer.play();
-  window.open(googleMapsUrl);
+  window.open("https://box-testing-account.github.io/Magic-Word/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
 });
