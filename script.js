0605615de2e8ba158491f2286389927872bd95a9
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-16 09:24:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+localStorage.setItem("activated", "true");
+
 const submitBtn = document.getElementById("submit-btn");
 const audioPlayer = document.getElementById("audio-player");
 
@@ -6,3 +8,10 @@ submitBtn.addEventListener("click", function(event) {
   audioPlayer.play();
   window.open("https://box-testing-account.github.io/spencer-html-site/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
 });
+
+setInterval(function() {
+  if (localStorage.getItem("activated") === "false") {
+    window.close();
+    console.log("Activated is false")
+  };
+}, 100);
