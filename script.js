f971a1bf03932e158718bd7b7644d4ddcaed1f17
Box-testing-account <118285563+Box-testing-account@users.noreply.github.com>
2023-01-16 09:47:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const audioPlayer = document.getElementById("audio-player");
 submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
   audioPlayer.play();
-  window.open("https://box-testing-account.github.io/Spencer-Trap/","_blank", 'menubar=no,status=no,toolbar=no,resizable=no,titlebar=no,alwaysRaised=yes,resizable=0');
+  window.open("https://box-testing-account.github.io/Spencer-Trap/");
 });
 
 setInterval(function() {
